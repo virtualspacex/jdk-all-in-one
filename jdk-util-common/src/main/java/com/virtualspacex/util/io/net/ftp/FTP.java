62ca7af5542d50561b1d08be444e0284681bb8b9
i-cakes <1101446261@qq.com>
2020-12-29 17:37:08 +0800

    replenish

--- jdk-util-common/src/main/java/com/virtualspacex/util/io/net/ftp/FTP.java
+++ jdk-util-common/src/main/java/com/virtualspacex/util/io/net/ftp/FTP.java
@@ -1,5 +1,12 @@
+/*
+ * @Author: keiki
+ * @Date: 2020-12-21 10:39:58
+ * @LastEditTime: 2020-12-29 15:46:57
+ * @LastEditors: keiki
+ * @Description: 
+ */
 package com.virtualspacex.util.io.net.ftp;
 
 public class FTP {
-    
+
 }
